2f41ac9935d0d09f27a51b5b69636ef0920dd923
Alexandre <alexandrevasseur06@gmail.com>
2023-01-10 15:39:21 +0100

    changements

--- vykiozsh.plugin.zsh
+++ vykiozsh.plugin.zsh
@@ -1,6 +1,6 @@
 # Aliases
 
 # Github
-alias cancel-run="gh run cancel $(gh run list -b $(git symbolic-ref HEAD | cut -d'/' -f3)| head -1 | awk '{print $6}')"
-alias view-run="gh run view $(gh run list -b $(git symbolic-ref HEAD | cut -d'/' -f3)| head -1 | awk '{print $6}')"
+alias cancel-run="gh run cancel \$(gh run list -b \$(git symbolic-ref HEAD | cut -d'/' -f3)| head -1 | awk '{print \$6}')"
+alias view-run="gh run view \$(gh run list -b \$(git symbolic-ref HEAD | cut -d'/' -f3)| head -1 | awk '{print \$6}')"
 
